26816fbe6ca9bdce81038c00336d68ac75d4b64c
Joko <edwardjptatuhas@outlook.com>
2022-03-06 19:11:06 +0700

    Adding C (again)

---
+++ Active/C/p001.c
@@ -0,0 +1,26 @@
+// Project Euler
+// Problem 1: The sum of multiple of 3 or 5 below 1000
+
+#include <stdio.h>
+
+int compute(int limit) {
+    int num = 1;
+    int sum = 0;
+
+    while (num < limit) {
+        if (num % 3 == 0 || num % 5 == 0) {
+            sum += num;
+        }
+
+        num += 1;
+    }
+
+    return sum;
+}
+
+int main(int argc, char *argv[]) {
+    int limit = 1000;
+    printf("%d\n", compute(limit));
+
+    return 0;
+}
