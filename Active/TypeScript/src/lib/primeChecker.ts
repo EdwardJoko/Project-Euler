f0a0dec2de41a0469f21e7fb901708609a59290d
Edo <edo@bfi.io>
2022-12-07 18:16:37 +0700

    little modification

--- Active/TypeScript/src/lib/primeChecker.ts
+++ Active/TypeScript/src/lib/primeChecker.ts
@@ -3,7 +3,7 @@ function primeChecker(num: number): boolean {
         return false;
     }
 
-    for (let i = 2; i * i <= num; i++) {
+    for (let i = 2; (i * i) <= num; i++) {
         if (num % i === 0) {
             return false;
         }
