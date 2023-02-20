1ea82dadcce287f1e7147c7772493928f51f2cd9
Edward J P Tatuhas <edwardjptatuhas@outlook.com>
2023-02-21 00:53:07 +0700

    Solve number 7 in C and also add is_prime library to C language

---
+++ Active/C/lib.h
@@ -0,0 +1,54 @@
+// to check if a number is a prime or not
+bool is_prime(unsigned long int number) {
+    if (number <= 1) return false;
+
+    for (unsigned long int i = 2; (i * i) <= number; i++) {
+        if (number % i == 0) return false;
+    }
+
+    return true;
+}
+
+// return an array of primes under the (limit+1)
+/*
+int* eratosthenes_sieve(int limit) {
+    if (limit <= 1) {
+        static int empty[] = [];
+        return empty;
+    }
+
+    // create an array with element from 0 to limit
+    int[] nums = new int[limit + 1];
+    for (int i = 0; i <= limit; i++) 
+        nums[i] = i;
+
+    // 1 is not prime
+    nums[1] = 0;
+
+    // eliminate non-prime numbers from the array with Eratosthenes's
+    // sieve
+    int nonZero = limit + 1; // the amount of non-zero numbers in array
+    for (int i = 0; i <= limit; i++) {
+        // skip already eliminated numbers
+        if (nums[i] == 0) {
+            nonZero -= 1;
+            continue;
+        }
+
+        for (int j = nums[i] * 2; j <= limit; j += nums[i])
+            nums[j] = 0;
+    }
+
+    // create new element that just include non-zero number from the
+    // nums array, or just only the primes
+    int[] primes = new int[nonZero];
+    for (int i = 0, j = 0; i <= limit; i++) {
+        if (nums[i] != 0) {
+            primes[j] = nums[i];
+            j += 1;
+        }
+    }
+
+    return primes;
+}
+*/
---
+++ Active/C/p007.h
@@ -0,0 +1,17 @@
+// Project Euler
+// Problem 7: The 10001st prime number
+
+#include <stdio.h>
+#include "lib.h"
+
+int compute007(int limit) {
+    int prime_th = 0;
+    int i = 0; // iterator variable
+
+    for (; prime_th != limit; i++) {
+        if (is_prime(i)) prime_th += 1;
+    }
+
+    // minus 1 because, i is getting added before the for loop stop
+    return (i - 1);
+}
