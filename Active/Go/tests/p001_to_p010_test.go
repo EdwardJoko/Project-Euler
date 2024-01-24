94056d514cfd053ed8fdfe01ccad4d9967ee16c5
Edward Joko <edwardjptatuhas@outlook.com>
2024-01-24 23:19:09 +0700

    Add test for number 4 in go

--- Active/Go/tests/p001_to_p010_test.go
+++ Active/Go/tests/p001_to_p010_test.go
@@ -65,3 +65,25 @@ func Test_003(t *testing.T) {
 		"Wrong solution for problem number 2",
 	)
 }
+
+func Test_004(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(
+		go_euler.Problem_004(2),
+		"9009",
+		"Wrong solution for problem number 4",
+	)
+
+	assert.Equal(
+		go_euler.Problem_004(3),
+		"906609",
+		"Wrong solution for problem number 4",
+	)
+
+	assert.Equal(
+		go_euler.Problem_004(4),
+		"99000099",
+		"Wrong solution for problem number 4",
+	)
+}
