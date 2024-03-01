a3f2217aceea56106d34ea38947b3c109e53d3bb
Edward Joko <edwardjptatuhas@outlook.com>
2024-03-01 14:37:37 +0700

    solve number 8 with golang

--- Active/Go/tests/p001_to_p010_test.go
+++ Active/Go/tests/p001_to_p010_test.go
@@ -165,3 +165,19 @@ func Test_007(t *testing.T) {
 		"Wrong solution for problem number 7",
 	)
 }
+
+func Test_008(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(
+		go_euler.Problem_008(4),
+		5832,
+		"Wrong solution for problem number 8",
+	)
+
+	assert.Equal(
+		go_euler.Problem_008(13),
+		23514624000,
+		"Wrong solution for problem number 8",
+	)
+}
